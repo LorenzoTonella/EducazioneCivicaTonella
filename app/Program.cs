dace81c1d09d0d64bd53b047506f799e46f16e81
LorenzoTonella <lorenzotonella2@gmail.com>
2023-04-05 13:49:20 +0200

    punto 2 semaforo verde

--- app/Program.cs
+++ app/Program.cs
@@ -4,9 +4,14 @@ namespace EsercizioGITv0._1
 {
     class Crittografia
     {
+        public static void Main()
+        {
+            PrimoPunto();
+            SecondoPunto();
+        }
         int counter=0;
         
-        public bool PrimoPunto()
+        public static bool PrimoPunto()
         {
             try{
             Console.Write("Inserisci una stringa: ");
@@ -37,9 +42,35 @@ namespace EsercizioGITv0._1
             }
                 
         }
-        public bool SecondoPunto()
+        public static bool SecondoPunto()
         {
-            return false;
+            try{
+                Console.Write("Inserisci una stringa: ");
+                string s = Console.ReadLine();
+                Console.Write("Inserisci un numero: ");
+                int n = int.Parse(Console.ReadLine());
+            // Punto 2
+                char[] arr2 = s.ToCharArray();
+                for (int i = 0; i < arr2.Length; i++)
+                {
+                    if (char.IsLetter(arr2[i]))
+                    {
+                        char c = (char)(arr2[i] - n);
+                        if (c < 'a')
+                            c = (char)(c + 26);
+                        arr2[i] = c;
+                    }
+                }
+                string result2 = new string(arr2);
+                Console.WriteLine($"Punto 2: {result2}");
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+                
         }
 
                
